3bc101acb5dcd234c691338c4a842e4ac5fa410d
jendralaulia <jendral.aulia@gmail.com>
2026-03-24 21:39:24 +0700

    Update and rename questions.js to songs.js

---
+++ songs.js
@@ -0,0 +1,63 @@
+// Bank Lagu Kuis Nada - Total 10 Lagu Testing
+const songBank = [
+    {
+        artist: "Dewa 19",
+        title: "Kangen",
+        artistOptions: ["Dewa 19", "Slank", "Padi", "Gigi"],
+        titleOptions: ["Kangen", "Pupus", "Risalah Hati", "Separuh Nafas"]
+    },
+    {
+        artist: "Sheila On 7",
+        title: "Dan...",
+        artistOptions: ["Sheila On 7", "Padi", "Naif", "Kla Project"],
+        titleOptions: ["Dan...", "Sephia", "Melompat Lebih Tinggi", "Kita"]
+    },
+    {
+        artist: "Coldplay",
+        title: "Yellow",
+        artistOptions: ["Coldplay", "Maroon 5", "Muse", "Keane"],
+        titleOptions: ["Yellow", "Fix You", "The Scientist", "Viva La Vida"]
+    },
+    {
+        artist: "Peterpan",
+        title: "Ada Apa Denganmu",
+        artistOptions: ["Peterpan", "Radja", "Ungu", "ST12"],
+        titleOptions: ["Ada Apa Denganmu", "Mungkin Nanti", "Bintang Di Surga", "Topeng"]
+    },
+    {
+        artist: "Michael Learns To Rock",
+        title: "That's Why You Go Away",
+        artistOptions: ["Michael Learns To Rock", "Westlife", "Backstreet Boys", "Boyzone"],
+        titleOptions: ["That's Why You Go Away", "Take Me To Your Heart", "Paint My Love", "Sleeping Child"]
+    },
+    {
+        artist: "Chrisye",
+        title: "Pergilah Kasih",
+        artistOptions: ["Chrisye", "Iwan Fals", "Ebiet G. Ade", "Fariz RM"],
+        titleOptions: ["Pergilah Kasih", "Anak Sekolah", "Kisah Kasih di Sekolah", "Cintaku"]
+    },
+    {
+        artist: "Oasis",
+        title: "Wonderwall",
+        artistOptions: ["Oasis", "Blur", "Nirvana", "Radiohead"],
+        titleOptions: ["Wonderwall", "Don't Look Back In Anger", "Creep", "Smells Like Teen Spirit"]
+    },
+    {
+        artist: "Bon Jovi",
+        title: "Livin' On A Prayer",
+        artistOptions: ["Bon Jovi", "Guns N' Roses", "Aerosmith", "Queen"],
+        titleOptions: ["Livin' On A Prayer", "Always", "It's My Life", "Sweet Child O' Mine"]
+    },
+    {
+        artist: "Letto",
+        title: "Ruang Rindu",
+        artistOptions: ["Letto", "Nidji", "D'Masiv", "Samsons"],
+        titleOptions: ["Ruang Rindu", "Sebelum Cahaya", "Sandaran Hati", "Permintaan Hati"]
+    },
+    {
+        artist: "Linkin Park",
+        title: "In The End",
+        artistOptions: ["Linkin Park", "Limp Bizkit", "Green Day", "Evanescence"],
+        titleOptions: ["In The End", "Numb", "Crawling", "Somewhere I Belong"]
+    }
+];
